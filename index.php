fdf558b8b30f012a0448a9291307a05957392e1a
mukku69 <programing0803@gmail.com>
2021-08-12 23:59:59 +0900

    wp_footer

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="http://linc.local:10004/wp-content/themes/My-blog/css/styles.css" rel="stylesheet" />
-    <?php wp head(); ?>
+    <?php wp_head(); ?>
     </head>
     <body>
         <!-- Navigation-->
@@ -152,5 +152,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="http://linc.local:10004/wp-content/themes/My-blog/js/scripts.js"></script>
+        <?php wp_footer(); ?>
     </body>
 </html>
