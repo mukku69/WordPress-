5bd20c3c7fa07c80217ae6b9ace25578b3a021d6
mukku69 <programing0803@gmail.com>
2021-08-14 23:59:59 +0900

    if

--- index.php
+++ index.php
@@ -52,12 +52,12 @@
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
+                    <?php if (have_posts()): ?>
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
                             <h2 class="post-title">
-                                <?php $title = the_title('■','●','false'); ?>
-                                <?php echo str_replace('新しい','古い',$title); ?>
+                                <?php the_title(); ?>
                             </h2>
                             <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
                         </a>
@@ -110,6 +110,9 @@
                     <hr class="my-4" />
                     <!-- Pager-->
                     <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
+                    <?php else: ?>
+                        <p>記事が見つかりませんでした</p>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
