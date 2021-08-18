a972eb332917941ab0550e296c8745d18b26fa8e
mukku69 <programing0803@gmail.com>
2021-08-18 23:59:59 +0900

    have_posts()を変数に代入

--- index.php
+++ index.php
@@ -52,7 +52,8 @@
         <div class="container px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
-                    <?php if (have_posts()): ?>
+                    <?php $lists = have_posts(); ?>
+                    <?php if ($lists): ?>
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
